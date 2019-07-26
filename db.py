953fbc37a2fbb1879b973331fdec725bea20b7da
dalduk <przemyslaw7wozny@gmail.com>
2019-07-26 17:59:06 +0200

    Update #1

--- db.py
+++ db.py
@@ -1,9 +1,13 @@
 import pyodbc
 import pandas as pd
+import numpy as np
 import plotly.express as px
+import plotly
 from datetime import date
 import urllib.request
 import json
+import orca
+import cv2
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-H4AEIGK;'
@@ -28,11 +32,10 @@ def newdb():
                 name = str(api["default_sku"]["entitlements"][0]["name"])
             except:
                 name = str(api["default_sku"]["eligibilities"][0]["name"])
-            name2 = name.replace("-", '').replace("’", '').replace("®",'').replace("'",'')
             today = date.today().strftime('%Y-%m-%d')
             img = str(api["images"][0]["url"])
-            string = "CREATE TABLE " +"[" + name2 +"] " +"( cena FLOAT NOT NULL,data DATE NOT NULL, url VARCHAR(2048) NOT NULL, img VARCHAR(2048) NOT NULL, )"
-            string2 ="[" + name2 +"] " +  "VALUES ("+ price2 +  ",'" + today + "'" + ",'https://store.playstation.com/pl-pl/product/"+ game +"','"+img+"')"
+            string = "CREATE TABLE " +"[" + name +"] " +"( cena FLOAT NOT NULL,data DATE NOT NULL, url VARCHAR(2048) NOT NULL, img VARCHAR(2048) NOT NULL, )"
+            string2 ="[" + name +"] " +  "VALUES ("+ price2 +  ",'" + today + "'" + ",'https://store.playstation.com/pl-pl/product/"+ game +"','"+img+"')"
             try:
                 cursor.execute(string + '''INSERT INTO ''' + string2)
             except:
@@ -52,21 +55,20 @@ def graph():
                 name = str(api["default_sku"]["entitlements"][0]["name"])
             except:
                 name = str(api["default_sku"]["eligibilities"][0]["name"])
-            name2 = name.replace("-", '').replace("’", '').replace("®", '').replace("'",'')
-        cursor.execute('''
-                            Select * from 
-                            '''+ " [" +name2+ "]")
+        cursor.execute('''Select * from '''+ " [" +name+ "]")
         rows = cursor.fetchall()
 
         df = pd.DataFrame([[ij for ij in i] for i in rows])
         df.rename(columns={0: 'cena', 1: 'data', 2: 'url', 3: 'img'}, inplace=True);
         df = df.sort_values(['data'], ascending=[1]);
 
-        fig = px.line(df, x="data", y="cena", title=name2)
-        fig.show()
+        fig = px.line(df, x="data", y="cena", title=name)
+        # fig.show()
+        img = fig.to_image()
+        nparr = np.fromstring(img, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        cv2.imshow(name,img_np)
+        cv2.waitKey(100)
+    cv2.waitKey(0)
 
 graph()
-
-
-
-
